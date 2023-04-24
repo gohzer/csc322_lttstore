1393003a901465f5918734ad2a4d66192f24a4ad
Jiazhou Zhang <jiazhou.zhang01@gmail.com>
2023-04-24 00:54:17 -0400

    new file:   pages/buildPC.tsx new file:   public/I9Core.jpg new file:   public/RTX2080.jpg new file:   public/motherboard.jpg ----------------------------------------- modified:   styles/globals.css Removed: body {   color: rgb(var(--foreground-rgb));   background: linear-gradient(       to bottom,       transparent,       rgb(var(--background-end-rgb))     )     rgb(var(--background-start-rgb)); } and
      --foreground-rgb: 0, 0, 0;
      --background-start-rgb: 214, 219, 220;
      --background-end-rgb: 255, 255, 255;

---
+++ pages/buildPC.tsx
@@ -0,0 +1,48 @@
+import Image from'next/image'
+import styles from '@/styles/Home.module.css'
+import RTX2080 from '../public/RTX2080.jpg'
+import motherboard from '../public/motherboard.jpg'
+import i9 from '../public/I9Core.jpg'
+import Link from 'next/link'
+
+export default function BuildPC() {
+    return (
+    <>
+        
+        <h1>
+        &nbsp;Welcome to the Macrocenter!&nbsp;
+          Build your PC here.
+        </h1>
+        
+        <body>
+            <h1>&nbsp;Please choose your component:&nbsp;</h1>
+            <ul>
+                <li>
+                    <h2>&nbsp;CPU: Intel&nbsp;| AMD</h2>
+                </li>
+                &nbsp;<Image src={i9} alt="Image" className={styles.image}
+               width={175}
+               height={175}
+                />
+                <li>
+                    <h2>&nbsp;Motherboard:</h2>
+                </li>
+                &nbsp;<Image src={motherboard} alt="Image" className={styles.image}
+              width={175}
+              height={175}
+                />
+                <li>
+                    <h2>&nbsp;Graphics card: Geforce RTX &nbsp;|
+                        AMD
+                    </h2>
+                </li>
+                &nbsp;<Image src={RTX2080} alt="Image" className={styles.image}
+               width={175}
+               height={175}
+            />
+            </ul>
+        </body>
+
+    </>
+    )
+}
