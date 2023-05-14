6056737b5005dc7a0c10b8666378ad64b7cfe3fa
rquit <nabilomi1@gmail.com>
2023-05-14 11:02:12 -0400

    fix navbar auth registration

--- pages/navbars.tsx
+++ pages/navbars.tsx
@@ -32,11 +32,9 @@ export default function Navbar() {
         setComponentList(list);
         auth.onAuthStateChanged(() => {
             if(auth.currentUser) {
+                var list = NavbarLinks.filter(p => p.name != "Sign In");
+                setComponentList(list);
                 if(!reload) checkEmployee();
-                if(reload) {
-                    var list = NavbarLinks.filter(p => p.name != "Sign In");
-                    setComponentList(list);
-                }
             }
         });
     }, [reload]);
